08945a5b76718b17b50a93f1d3c45f5651ee3e0a
Vincent Young <vincent_young@apple.com>
2017-01-29 10:58:01 -0800

    Final video2 changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Whatever' });
+  res.render('shop/index', { title: 'Whatever' });
 });
 
 module.exports = router;
