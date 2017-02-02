95e2ab0e383ee26d07c9a57d07b84aeb3faee6aa
Vincent Young <vincent_young@apple.com>
2017-02-02 09:51:46 -0800

    video5

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 var Product = require('../models/product');
 var csrf = require('csurf');
 
@@ -22,13 +23,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/user/signup', function(req,res,next) {
+    var messages = req.flash('error');
     res.render('user/signup',{
-      csrfToken:req.csrfToken()
+      csrfToken:req.csrfToken(),
+      messages:messages,
+      hasErrors:messages.length > 0
     });
 });
 
-router.post('/user/signup', function(req,res,next) {
-  res.redirect('/');
+router.post('/user/signup', passport.authenticate('local.signup', {
+  successRedirect:'/user/profile',
+  failureRedirect:'/user/signup',
+  failureFlash:true
+}));
+
+router.get('/user/profile', function(req,res,next) {
+  res.render('user/profile');
 });
 
 module.exports = router;
