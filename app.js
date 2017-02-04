91f44ea6df185d1825989bca9bbe9c29aa7a6259
Vincent Young <vincent_young@apple.com>
2017-02-04 10:43:18 -0800

    video9

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var index = require('./routes/index');
 var userRoutes = require('./routes/user');
@@ -37,7 +38,13 @@ app.use(cookieParser());
 app.use(session({
   secret:'mysupersecret',
   resave:false,
-  saveUninitialized:false
+  saveUninitialized:false,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  }),
+  cookie: {
+    maxAge:180 * 60 * 1000
+  }
 }));
 app.use(flash());
 app.use(passport.initialize());
@@ -46,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
   next();
 });
 
